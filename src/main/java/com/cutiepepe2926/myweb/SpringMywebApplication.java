39dd48ad8defc63d11dc6a07442a2a99aaf66823
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-10-31 17:19:45 +0900

    feat(topic) : 토픽 등록 기능 구현 완료

---
+++ src/main/java/com/cutiepepe2926/myweb/SpringMywebApplication.java
@@ -0,0 +1,13 @@
+package com.cutiepepe2926.myweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMywebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cutiepepe2926/myweb/SpringMywebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cutiepepe2926.myweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMywebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
